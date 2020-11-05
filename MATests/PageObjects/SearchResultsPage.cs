66ed457200b2ab7611e6fb5800b64adfe42dce1b
Faronnnn <52966054+Faronnnn@users.noreply.github.com>
2020-11-05 01:37:06 +0100

    SearchresultsPage

---
+++ MATests/PageObjects/SearchResultsPage.cs
@@ -0,0 +1,18 @@
+﻿using MATests.PageObjects.Base;
+using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MATests.PageObjects
+{
+    class SearchResults : PageWithPagination
+    {
+        public SearchResults(IWebDriver driver) : base(driver)
+        { }
+
+        private IReadOnlyCollection<IWebElement> _results => _driver.FindElements(By.CssSelector("latest_post_custom"));
+
+        public int GetNumberOfResults() => _results.Count;
+    }
+}
